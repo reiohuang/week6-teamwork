b86cf05defbe8dc7ad9b4358328d48e7e412abfc
reio <reiokai@gmail.com>
2020-07-26 20:38:38 +0800

    delete App.vue semicolon

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
+    <loading :active.sync="isLoading"></loading>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 預設關閉
+      isLoading: false
+    }
+  },
+  mounted () {
+    // 畫面準備完打開
+    this.isLoading = true
+    // 2秒後關閉
+    setTimeout(() => {
+      this.isLoading = false
+    }, 2000)
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
